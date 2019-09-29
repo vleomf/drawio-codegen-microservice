b2aa996032e492d01b3cc7e9aa07e94ca6710351
Leopoldo Muñoz <viceo00@gmail.com>
2019-09-28 23:14:50 -0500

    Definicion de Interfaz para archivos MXFile

---
+++ app/Adapters/IMXFileAdapter.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Adapters;
+
+/**
+ ** Interfaz para lectura de archivos 'mxDiagram' (drawio) 
+ */
+interface IMXFileAdapter
+{
+    /**
+     ** Metodo que calcula el total de paginas en documento 'mx; 
+     *  @return int numero de paginas
+     */
+    public function totalPages() : int;
+
+    /**
+     ** Metodo que calcula el total de nodos en una pagina del documento
+     *  @param  int pagina del documento
+     *  @return int numero de nodos 
+     */
+    public function totalNodesFromPage(int $page) : int;
+
+    /**
+     ** Metodo para obtener nodo del documento por pagina
+     *  @param  int     pagina del documento
+     *  @param  int     posicion del nodo
+     *  @return MxNode  nodo
+     */
+    public function getMXCell(int $page, int $nodePosition) : IMXCell;
+}
