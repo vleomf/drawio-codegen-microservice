e6c613ad1362f79af5fdc03cbf94951e7c33bedd
Leopoldo Muñoz <viceo00@gmail.com>
2019-10-20 23:01:13 -0500

    Subiendo cambios en controlador, se siguen haciendo pruebas... etapa de parseo de valores en nodos SimpleXmlElement... debemos verificar por expresion regular si el elemento es un atributo o un metodo .. esto es en la clase MXHardClassifier

--- app/Http/Controllers/RPCController.php
+++ app/Http/Controllers/RPCController.php
@@ -4,17 +4,26 @@ namespace App\Http\Controllers;
 use Laravel\Lumen\Routing\Controller;
 use Illuminate\Http\Request;
 use App\Adapters\MXFile;
+use App\Strategies\MXHardClassifier;
 
 class RPCController extends Controller
 {
-    private $xmlFileStrategy;
+    private $xmlFileAdapter;
+    private $nodes;
+    private $classifier;
 
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct(){}
+    public function __construct(){
+        //  Inicializamos lista para guardar nodos que
+        //  nos genera el Adaptador de archivo MXFile
+        $this->nodes = [];
+        //  Definimos el clasificador a ocupar
+        $this->classifier = new MXHardClassifier();
+    }
 
     public function health(Request $request) {
         return "ok";
@@ -40,12 +49,51 @@ class RPCController extends Controller
     }
 
     private function GenerarPHP( $xml ) {
-        $this->xmlFileStrategy = new MXFile(new \SimpleXmlElement( $xml ));
-        $totalPaginas = $this->xmlFileStrategy->totalPages();
-        for($i = 0; $i<$totalPaginas; $i++){
-            $totalNodosPorPagina = $this->xmlFileStrategy->totalNodesFromPage($i);
-            print_r($totalNodosPorPagina); die;
+        //  Cargamos cadena XML en el Adaptador
+        $this->xmlFileAdapter = new MXFile(new \SimpleXmlElement( $xml ));
+        
+        //  Obtenemos el total de paginas dentro del XML 
+        //  Nota: El diagrama de Drawoio genera los xml por pagina
+        //  se debe verificar que exista pagina alguna que procesar.
+        $totalPaginas = $this->xmlFileAdapter->totalPages();
+        
+        //  Iteramos las paginas del diagrama y obtenemos el total de nodos
+        //  por pagina. Estos nodos procesaran para crear nodos usando la fabrica
+        for($i = 0; $i<$totalPaginas; $i++) 
+        {
+            //  Obtenemos el total de nodos por pagina.
+            $totalNodosPorPagina = $this->xmlFileAdapter->totalNodesFromPage($i);
+            
+            //  Iteramos nodo por nodo
+            for( $ii = 0; $ii < $totalNodosPorPagina; $ii++ )
+            {
+                //  Insertamos elementos MXCell en la lista de nodos
+                $this->nodes[] = $this->xmlFileAdapter->getMXCell( $i, $ii );
+            }    
         }
+
+        //  Hay que clasificar los nodos y generar los objetos desde la fabrica
+        //  Cada objeto puede ser, o clase, o atributo, o relacion entre clases.. etc
+        //  Todo esto en funcion de los diagramas de clase UML.
+
+        //var_dump($this->nodes); die;
+        foreach($this->nodes as $node) 
+        {
+            $clasificacion = $this->classifier->classify($node);
+
+            var_dump($node['id'], $node['value']);
+            var_dump($clasificacion); echo "<br />";
+            // switch($clasificacion) 
+            // {
+            //     case "global" :
+            //         var_dump($node);
+            //         //var_dump($node["value"], $node["style"]);
+            //     break;
+            // }
+        }
+        
+        die;
+
         
         print_r($totalPaginas); die;
     }
